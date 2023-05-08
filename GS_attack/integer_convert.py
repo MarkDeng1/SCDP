884a4f00bee51012d5cdacb1ccddd3d6735d0545
MarkDeng1 <114970603+MarkDeng1@users.noreply.github.com>
2023-05-09 03:24:54 +1000

    Add files via upload

---
+++ GS_attack/integer_convert.py
@@ -0,0 +1,135 @@
+import struct
+from xmlrpc.client import Binary
+import numpy as np
+import torch
+
+def convert_to_binary(num):
+    num1 = num
+    num = round(num.item(), 5) # 保留小数点后5位
+    # try: 
+
+    #     int(num * 100000)
+    # except Exception as e:
+    #     print('num:', num1)
+        
+    
+    num = int(num * 100000) # 乘以100000并转换为整数
+    if num >= 0:
+        binary_number = bin(num)[2:].zfill(32)
+    else:
+        binary_number = bin(num)[3:].zfill(32)
+        binary_number = '1' + binary_number[1:]
+    return binary_number
+
+
+
+def binary_to_decimal(binary):
+    
+    if binary[0] == '1':
+        binary1 = '0' + binary[1:]
+        decimal_number = -int(binary1, 2) 
+    else:
+        decimal_number = int(binary, 2)
+    decimal_number /= 100000
+    return decimal_number
+
+
+# 下面的function把任意一个tensor转化成内部为32 binary
+def binary_convert(tensor, p):
+    binary_tensor = np.array([convert_to_binary(x) for x in tensor.flatten()])
+    binary_tensor = list(binary_tensor)
+    binary_tensor2 = []
+    for i, to_binary in enumerate(binary_tensor):
+        binary_list = list(to_binary)
+        for j, str_ in enumerate(binary_list):
+            if np.random.random() > p and j in {21,22}:
+                binary_list[j] = str(1-int(str_))
+        new_str = ''.join(binary_list)
+
+        binary_tensor2.append(binary_to_decimal(new_str))
+    binary_tensor2 = np.array(binary_tensor2).reshape(tensor.shape)
+    return binary_tensor2
+
+def rearrange_tensor_list(tensor_list):
+    new_list = []
+    stacked_tensors = np.stack(tensor_list , axis=0)
+    if np.array(tensor_list).ndim == 5:
+        for i in range(stacked_tensors.shape[1]):
+            for j in range(stacked_tensors.shape[2]):
+                for k in range(stacked_tensors.shape[3]):
+                    for l in range(stacked_tensors.shape[4]):
+                    # 获取相同位置的元素并添加到新列表中
+                        new_list.append(stacked_tensors[:, i, j, k, l])  
+    if np.array(tensor_list).ndim == 2:
+        for i in range(stacked_tensors.shape[1]):
+            new_list.append(stacked_tensors[:, i])
+    
+    if np.array(tensor_list).ndim == 4:
+        for i in range(stacked_tensors.shape[1]):
+            for j in range(stacked_tensors.shape[2]):
+                for k in range(stacked_tensors.shape[3]):
+                    new_list.append(stacked_tensors[:, i, j, k])
+    
+    if np.array(tensor_list).ndim == 3:
+        for i in range(stacked_tensors.shape[1]):
+            for j in range(stacked_tensors.shape[2]):
+                new_list.append(stacked_tensors[:, i, j])
+    
+    return new_list
+
+# 把binary 32的tensor还原
+def process_tensors(tensor_list,p):
+    num_tensors = len(tensor_list)
+    num_bits = 32
+    new_list = rearrange_tensor_list(tensor_list)
+    result = []
+
+    for sublist in new_list:
+        new_values = []
+        for bit_index in range(num_bits):
+            
+            new_num = 0
+            new_int = 0
+            new_str = ''
+            new_char = ''
+            value = 0.0
+            
+            for j in range(len(sublist)):
+                try: 
+
+                    sublist[j][bit_index] == '1'
+                except Exception as e:
+                    print('j:', j, "bit_index: ", bit_index)
+                    print(sublist)
+                
+                if sublist[j][bit_index] == '1':
+                    new_num += 1
+                if j == len(sublist) - 1:
+                    new_num = new_num / ( p  * num_tensors)
+                    if new_num < 0.5:
+                        new_char = '0'
+                    else:
+                        new_char = '1'
+
+          
+        #   new_str += new_char
+            new_values.append(new_char)
+          
+        new = ''.join(new_values)
+        result.append(new)
+    
+    result2 = []
+    for item in result:
+        
+        result2.append(binary_to_decimal(item))
+    
+    source_tensor = torch.tensor(result2)
+    if source_tensor.numel() != 1 and source_tensor.numel() != 0:
+        reshaped_tensor = source_tensor.reshape(tensor_list[0].shape)
+        reshaped_tensor.requires_grad = True
+    else:
+        reshaped_tensor = source_tensor 
+        reshaped_tensor.requires_grad = True
+    return reshaped_tensor
+
+
---
+++ GS_attack/inversefed/options.py
@@ -0,0 +1,92 @@
+"""Parser options."""
+
+import argparse
+
+def options():
+    """Construct the central argument parser, filled with useful defaults."""
+    parser = argparse.ArgumentParser(description='Reconstruct some image from a trained model.')
+
+    # Central:
+    parser.add_argument('--model', default='ConvNet', type=str, help='Vision model.')
+    parser.add_argument('--dataset', default='CIFAR10', type=str)
+    parser.add_argument('--dtype', default='float', type=str, help='Data type used during reconstruction [Not during training!].')
+
+
+    parser.add_argument('--trained_model', action='store_true', help='Use a trained model.')
+    parser.add_argument('--epochs', default=120, type=int, help='If using a trained model, how many epochs was it trained?')
+
+    parser.add_argument('--accumulation', default=0, type=int, help='Accumulation 0 is rec. from gradient, accumulation > 0 is reconstruction from fed. averaging.')
+    parser.add_argument('--num_images', default=1, type=int, help='How many images should be recovered from the given gradient.')
+    parser.add_argument('--target_id', default=None, type=int, help='Cifar validation image used for reconstruction.')
+    parser.add_argument('--label_flip', action='store_true', help='Dishonest server permuting weights in classification layer.')
+
+    # Rec. parameters
+    parser.add_argument('--restarts', default=1, type=int, help='How many restarts to run.')
+    parser.add_argument('--cost_fn', default='sim', type=str, help='Choice of cost function.')
+    parser.add_argument('--indices', default='def', type=str, help='Choice of indices from the parameter list.')
+    parser.add_argument('--weights', default='equal', type=str, help='Weigh the parameter list differently.')
+
+    parser.add_argument('--optimizer', default='adam', type=str, help='Weigh the parameter list differently.')
+    parser.add_argument('--signed', action='store_false', help='Do not used signed gradients.')
+    parser.add_argument('--boxed', action='store_false', help='Do not used box constraints.')
+
+    parser.add_argument('--scoring_choice', default='loss', type=str, help='How to find the best image between all restarts.')
+    parser.add_argument('--init', default='randn', type=str, help='Choice of image initialization.')
+    parser.add_argument('--tv', default=1e-4, type=float, help='Weight of TV penalty.')
+
+    # SCDP
+    # quantization arguments
+    parser.add_argument('--quantization', type=bool, default=True,
+                        help="whether to perform quantization")
+    parser.add_argument('--binary', type=bool, default=True,
+                        help="whether to perform binary convert")
+    parser.add_argument('--lattice_dim', type=int, default=2,
+                        choices=[1, 2], 
+                        help="perform scalar (lattice_dim=1) or lattice (lattice_dim=2) quantization ")
+    parser.add_argument('--R', type=int, default=16,
+                        help="compression rate (number of bits per sample)")
+    parser.add_argument('--gamma', type=float, default=set_gamma(parser.parse_args()),
+                        help="quantizer dynamic range")
+    parser.add_argument('--vec_normalization', type=bool, default=set_vec_normalization(parser.parse_args()),
+                        help="whether to perform vectorized normalization, otherwise perform scalar normalization")
+
+
+
+    # Files and folders:
+    parser.add_argument('--save_image', action='store_true', help='Save the output to a file.')
+
+    parser.add_argument('--image_path', default='images_new/', type=str)
+    parser.add_argument('--model_path', default='models/', type=str)
+    parser.add_argument('--data_path', default='~/data', type=str)
+
+    # Debugging:
+    parser.add_argument('--name', default='iv', type=str, help='Name tag for the result table and model.')
+    parser.add_argument('--deterministic', action='store_true', help='Disable CUDNN non-determinism.')
+    parser.add_argument('--dryrun', action='store_true', help='Run everything for just one step to test functionality.')
+
+    # Defense strategy:
+    parser.add_argument('--defense', default='no', type=str, help='defense strategy.')
+    parser.add_argument('--pruning_rate', default=0, type=float, help='pruning rate for defense.')
+    
+    return parser
+
+def set_gamma(args):
+    eta = 1.5
+    gamma = 1
+    if args.privacy:
+        if args.lattice_dim == 1:
+            gamma += 2 * ((2 / args.epsilon) ** 2)
+        else:
+            gamma += args.sigma_squared * (args.nu / (args.nu - 2))
+    return eta * np.sqrt(gamma)
+
+
+def set_vec_normalization(args):
+    vec_normalization = False
+    if args.quantization:
+        if args.lattice_dim == 2:
+            vec_normalization = True
+    if args.privacy:
+        if args.privacy_noise == 't' or args.privacy_noise == 'jopeq_vector':
+            vec_normalization = True
+    return vec_normalization
